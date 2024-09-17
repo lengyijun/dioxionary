1936d9b796040e66cf4fa57a0478b50025c72519
lengyijun <sjtu5140809011@gmail.com>
2024-09-17 13:46:12 +0800

    rm unworkable code

--- src/history.rs
+++ src/history.rs
@@ -6,9 +6,6 @@ use prettytable::{Attr, Cell, Row, Table};
 use rusqlite::Connection;
 use std::fs::create_dir;
 
-/// Allowed diffculty level types of a word.
-pub static ALLOWED_TYPES: [&str; 7] = ["CET4", "CET6", "TOEFL", "IELTS", "GMAT", "GRE", "SAT"];
-
 /// Check and generate cache directory path.
 pub fn get_db() -> Result<Connection> {
     let mut path = data_dir().with_context(|| "Couldn't find data directory")?;
@@ -69,66 +66,11 @@ pub fn add_history(word: String) -> Result<()> {
 /// | 220  | 305  | 207   | 203   | 142  | 242 | 126 |
 /// +------+------+-------+-------+------+-----+-----+
 ///
-pub fn list_history(type_: Option<String>, sort: bool, table: bool, column: usize) -> Result<()> {
-    let mut stmt = "SELECT WORD, DATE FROM HISTORY".to_string();
-
-    if let Some(type_) = type_ {
-        if ALLOWED_TYPES.contains(&type_.as_str()) {
-            stmt.push_str(format!(" WHERE {} = 1", type_).as_str())
-        }
-    }
-
-    let conn = get_db()?;
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let word_iter = stmt.query_map([], |row| row.get(0) as rusqlite::Result<String>)?;
-
-    let mut words: Vec<String> = word_iter.filter_map(|x| x.ok()).collect();
-
-    if sort {
-        words.sort();
-    }
-
-    if table {
-        let mut table = Table::new();
-        words.chunks(column).for_each(|x| {
-            table.add_row(x.iter().map(|x| Cell::new(x)).collect());
-        });
-        table.printstd();
-    } else {
-        words.into_iter().for_each(|x| {
-            println!("{}", x);
-        });
-    }
-
-    Ok(())
+pub fn list_history(ttype: Option<String>, sort: bool, table: bool, column: usize) -> Result<()> {
+    unreachable!()
 }
 
 /// Count the history.
 pub fn count_history() -> Result<()> {
-    let conn = get_db()?;
-
-    let header: Row = ALLOWED_TYPES
-        .into_iter()
-        .map(|x| Cell::new(x).with_style(Attr::Bold))
-        .collect();
-
-    let mut table: Table = Table::new();
-    table.add_row(header);
-
-    let body: Row = ALLOWED_TYPES
-        .into_iter()
-        .map(|x| {
-            let stmt = format!("SELECT COUNT(*) FROM HISTORY WHERE {} = 1", x);
-            let mut stmt = conn.prepare(&stmt).unwrap();
-            let res = stmt.query_row([], |row| row.get(0) as rusqlite::Result<i32>);
-            Cell::new(&res.unwrap().to_string())
-        })
-        .collect();
-
-    table.add_row(body);
-
-    table.printstd();
-
-    Ok(())
+    unreachable!()
 }
